aa5600e69aab3d957281bea5c4c18f1e82b76820
nitishgoswami-git <itsgoswaminitish@gmail.com>
2026-07-03 00:11:04 +0530

    Remove generated build artifacts

--- frontend/dev/static/chunks/app_page_tsx_000lpn1._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/_0f-k0p4._.js"],
-    source: "dynamic"
-});
